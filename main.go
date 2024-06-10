81caec88a08479235ad92e713a37b28a1bfe9412
Jason Chavannes <jason.chavannes@gmail.com>
2024-06-10 15:16:27 -0700

    Collect stats on per minute basis and continuously send to Influx.

--- main.go
+++ main.go
@@ -16,31 +16,41 @@ func main() {
 		log.Fatalf("error loading config; %v", err)
 	}
 	influx.SetInfluxWriter(cfg.Influx)
-	var stats1 = map[string]*sys.Stats{}
-	for _, block := range cfg.Blocks {
-		stats1[block.Name] = sys.GetStats(cfg.Connect, block.Name)
-	}
-	time.Sleep(10 * time.Second)
-	var stats2 = map[string]*sys.Stats{}
-	for _, block := range cfg.Blocks {
-		stats2[block.Name] = sys.GetStats(cfg.Connect, block.Name)
-	}
-	for block1, stat1 := range stats1 {
-		for block2, stat2 := range stats2 {
-			if block1 == block2 {
-				diff := stat1.Diff(stat2)
-				log.Printf("Diff for: %s\n", block2)
-				diff.Output()
-				influx.AddIoStat(block2, map[string]interface{}{
-					"ReadsCompleted":  diff.ReadsCompleted,
-					"ReadTime":        diff.ReadTime,
-					"WritesCompleted": diff.WritesCompleted,
-					"WriteTime":       diff.WriteTime,
-					"IOTime":          diff.IOTime,
-					"WeightedIOTime":  diff.WeightedIOTime,
-				})
+	ticker := time.NewTicker(60 * time.Second)
+	var statsPrev = make(map[string]*sys.Stats)
+	log.Printf("Starting sysstat\n")
+	for loop := 0; ; loop++ {
+		var statsNew = make(map[string]*sys.Stats)
+		for _, block := range cfg.Blocks {
+			statsNew[block.Name] = sys.GetStats(cfg.Connect, block.Name)
+		}
+		for blockNew, statNew := range statsNew {
+			for blockPrev, statPrev := range statsPrev {
+				if blockNew == blockPrev {
+					diff := statPrev.Diff(statNew)
+					if loop%10 == 1 {
+						log.Printf("Diff for: %s\n", blockNew)
+						diff.Output()
+					}
+					influx.AddIoStat(blockNew, map[string]interface{}{
+						"ReadsCompleted":           diff.ReadsCompleted,
+						"ReadTime":                 diff.ReadTime,
+						"WritesCompleted":          diff.WritesCompleted,
+						"WriteTime":                diff.WriteTime,
+						"IOTime":                   diff.IOTime,
+						"WeightedIOTime":           diff.WeightedIOTime,
+						"ReadsCompletedPerSecond":  float32(diff.ReadsCompleted) / 60.0,
+						"ReadTimePercent":          float32(diff.ReadTime) / 60000.0,
+						"WritesCompletedPerSecond": float32(diff.WritesCompleted) / 60.0,
+						"WriteTimePercent":         float32(diff.WriteTime) / 60000.0,
+						"IOTimePercent":            float32(diff.IOTime) / 60000.0,
+						"WeightedIOTimePercent":    float32(diff.WeightedIOTime) / 60000.0,
+					})
+				}
 			}
 		}
+		influx.Flush()
+		statsPrev = statsNew
+		<-ticker.C
 	}
-	influx.Flush()
 }
